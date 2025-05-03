fdec8a31871ca4d06c1c65e9c61620c292c398f2
Natnael <nsisay49@gmail.ocm>
2025-05-03 07:35:25 -1200

    home info filled

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,50 @@
 import { ScrollView, View } from "react-native";
-import React from "react";
-import { Link } from "expo-router"; // Import Link from expo-router
+import React, { useEffect, useState } from "react";
+import { Link, router } from "expo-router";
 import { Sidebar } from "@/components/Sidebar";
 import { Heading } from "@/components/ui/heading";
 import { Text } from "@/components/ui/text";
 import { Button, ButtonIcon } from "@/components/ui/button";
 import { GripVerticalIcon } from "@/components/ui/icon";
-import { Alert, AlertText } from "@/components/ui/alert";
-import { Progress, ProgressFilledTrack } from "@/components/ui/progress";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Center } from "@/components/ui/center";
 
 const Index = () => {
+  interface UserInfo {
+    fullName?: string;
+    isInformed?: boolean;
+    department?: string;
+    gender?: string;
+    year?: string;
+  }
+
+  const [userInfo, setUserInfo] = useState<UserInfo>({});
+  const [firstName, setFirstName] = useState("");
+
+  useEffect(() => {
+    const checkInformedStats = async () => {
+      const userdata = await AsyncStorage.getItem("userData");
+      if (userdata) {
+        const parsedData = JSON.parse(userdata);
+        setUserInfo(parsedData);
+
+        // Extract the first name from fullName
+        const fullName = parsedData.fullName || "";
+        const firstName = fullName.split(" ")[0];
+
+        setFirstName(firstName);
+
+        if (parsedData.isInformed) {
+          router.push("/(tabs)");
+        } else {
+          router.push("/(screens)/studentInfo");
+        }
+      }
+    };
+
+    checkInformedStats();
+  }, []);
+
   return (
     <ScrollView
       contentContainerStyle={{
@@ -24,7 +58,7 @@ const Index = () => {
       <View className="flex-row justify-between items-center p-4 bg-white w-full">
         <View className="gap-2 flex-row items-center w-full">
           <Sidebar />
-          <Heading size="lg">Asu Students App</Heading>
+          <Heading size="lg">ASU Students App</Heading>
         </View>
         <Button variant="link">
           <ButtonIcon as={GripVerticalIcon} />
@@ -34,43 +68,23 @@ const Index = () => {
       {/* Welcome Section */}
       <View className="bg-white w-[90vw] mt-6 rounded-lg shadow-md p-3">
         <View className="p-5 flex-col gap-3">
-          <Heading size="lg">Welcome back, Alex</Heading>
+          <Heading size="lg">Welcome back, {firstName || "Guest"}</Heading>
           <Text>
             Track your academic progress and campus life all in one place.
           </Text>
           <View className="flex-row justify-between items-center gap-3">
             <Text className="text-white bg-primary-700 rounded-full px-3 py-1">
-              CS
+              {userInfo.department}
+            </Text>
+            <Text className="text-white bg-primary-700 rounded-full px-3 py-1 w-20">
+              Year: {userInfo.year}
             </Text>
             <Text className="text-white bg-primary-700 rounded-full px-3 py-1">
-              Year 3
-            </Text>
-            <Text className="text-white bg-primary-700 rounded-full px-3 py-1">
-              FT
+              {userInfo.gender}
             </Text>
           </View>
         </View>
       </View>
-
-      {/* Library Section */}
-      <View className="bg-white w-[90vw] mt-6 rounded-lg shadow-md p-5">
-        <View className="flex-row justify-between items-center">
-          <Heading>Library Extended Hours</Heading>
-          <Alert action="info" variant="solid" className="rounded-full">
-            <AlertText>New</AlertText>
-          </Alert>
-        </View>
-        <Text className="mb-4">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam vel
-          nesciunt, perferendis recusandae eos vitae repellat voluptates porro
-          ipsa quidem sequi officia dolorem autem veritatis iure culpa aliquam,
-          minima eligendi.
-        </Text>
-        <Progress value={40} size="md" orientation="horizontal">
-          <ProgressFilledTrack />
-        </Progress>
-      </View>
-
       {/* Sections */}
       <View className="mb-20">
         {/* Curriculum */}
