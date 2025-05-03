82934e4d2157d86a5c3311958e7066fa10b1b3d6
Natnael <nsisay49@gmail.ocm>
2025-05-03 06:51:23 -1200

    student Info saved

--- app/(screens)/studentInfo/index.tsx
+++ app/(screens)/studentInfo/index.tsx
@@ -1,22 +1,10 @@
-import { View } from "react-native";
-import React from "react";
+import { View, Alert } from "react-native";
+import React, { useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Text } from "@/components/ui/text";
 import { Heading } from "@/components/ui/heading";
 import { Input, InputField } from "@/components/ui/input";
 import { Button, ButtonText } from "@/components/ui/button";
-import {
-  Actionsheet,
-  ActionsheetBackdrop,
-  ActionsheetContent,
-  ActionsheetDragIndicator,
-  ActionsheetDragIndicatorWrapper,
-  ActionsheetFlatList,
-  ActionsheetItem,
-  ActionsheetItemText,
-  ActionsheetSectionHeaderText,
-  ActionsheetSectionList,
-} from "@/components/ui/actionsheet";
-import { Link } from "expo-router";
 import {
   Select,
   SelectBackdrop,
@@ -32,38 +20,60 @@ import {
 import { ChevronDownIcon } from "@/components/ui/icon";
 
 const StudentInfo = () => {
-  const [showActionsheet, setShowActionsheet] = React.useState(false);
-  const handleClose = () => setShowActionsheet(false);
-  const DATA = [
-    {
-      title: "Gender",
-      data: ["Men", "Women", "Boy", "Girl"],
-    },
-  ];
+  const [fullName, setFullName] = useState("");
+  const [gender, setGender] = useState("");
+  const [department, setDepartment] = useState("");
+  const [year, setYear] = useState("");
+  const [isInformed, setIsInformed] = useState(false);
+
+  const handleSubmit = async () => {
+    try {
+      const studentInfo = {
+        fullName,
+        gender,
+        department,
+        year,
+      };
+      await AsyncStorage.setItem("userData", JSON.stringify(studentInfo));
+      Alert.alert("Success", "Student information saved successfully!");
+      if (studentInfo !== null && studentInfo !== undefined) {
+        setIsInformed(true);
+      }
+    } catch (error) {
+      Alert.alert("Error", "Failed to save student information.");
+    }
+  };
 
   return (
     <View className="flex-1 justify-center items-center">
-      <View className="bg-white w-[90vw] shadow-md rounded-lg px-3 gap-5 pt-5 pb-10 ">
+      <View className="bg-white w-[90vw] shadow-md rounded-lg px-3 gap-5 pt-5 pb-10">
         <Heading size="xl" className="pb-5">
           Student Information
         </Heading>
 
+        {/* Full Name */}
         <View>
           <Text size="sm">Full Name</Text>
           <Input variant="rounded">
-            <InputField placeholder="john doe" />
+            <InputField
+              placeholder="john doe"
+              value={fullName}
+              onChangeText={(fullName) => setFullName(fullName)}
+            />
           </Input>
         </View>
 
+        {/* Gender */}
         <View>
           <Text size="sm" className="w-40">
             Gender
           </Text>
-          <Select>
+          <Select onValueChange={(value) => setGender(value)}>
             <SelectTrigger>
               <SelectInput
                 placeholder="Select your Gender"
                 className="flex-1 py-1"
+                value={gender}
               />
               <SelectIcon className="mr-3" as={ChevronDownIcon} />
             </SelectTrigger>
@@ -73,11 +83,6 @@ const StudentInfo = () => {
                 <SelectDragIndicatorWrapper>
                   <SelectDragIndicator />
                 </SelectDragIndicatorWrapper>
-                <SelectItem
-                  label="Select your Gender"
-                  value="pink"
-                  isDisabled={true}
-                />
                 <SelectItem label="Male" value="male" />
                 <SelectItem label="Female" value="female" />
               </SelectContent>
@@ -85,15 +90,17 @@ const StudentInfo = () => {
           </Select>
         </View>
 
+        {/* Department */}
         <View>
           <Text size="sm" className="w-40">
             Department
           </Text>
-          <Select>
+          <Select onValueChange={(value) => setDepartment(value)}>
             <SelectTrigger>
               <SelectInput
                 placeholder="Select your Department"
                 className="flex-1 py-1"
+                value={department}
               />
               <SelectIcon className="mr-3" as={ChevronDownIcon} />
             </SelectTrigger>
@@ -103,30 +110,27 @@ const StudentInfo = () => {
                 <SelectDragIndicatorWrapper>
                   <SelectDragIndicator />
                 </SelectDragIndicatorWrapper>
-                <SelectItem label="Remedial" value="remedial" />
-                <SelectItem label="Fresh" value="fresh" />
                 <SelectItem label="Computer Science" value="CS" />
                 <SelectItem label="Information Technology" value="IT" />
                 <SelectItem label="Law" value="law" />
-                <SelectItem label="Information systems" value="IS" />
-                <SelectItem label="Electrical Engineering" value="EE" />
                 <SelectItem label="Economics" value="economics" />
-                <SelectItem label="Journalism" value="journal" />
                 <SelectItem label="Others" value="other" />
               </SelectContent>
             </SelectPortal>
           </Select>
         </View>
 
+        {/* Year */}
         <View>
           <Text size="sm" className="w-40">
             Year
           </Text>
-          <Select>
+          <Select onValueChange={(value) => setYear(value)}>
             <SelectTrigger>
               <SelectInput
-                placeholder="Select select a year"
+                placeholder="Select a year"
                 className="flex-1 py-1"
+                value={year}
               />
               <SelectIcon className="mr-3" as={ChevronDownIcon} />
             </SelectTrigger>
@@ -136,27 +140,19 @@ const StudentInfo = () => {
                 <SelectDragIndicatorWrapper>
                   <SelectDragIndicator />
                 </SelectDragIndicatorWrapper>
-                <SelectItem
-                  label="Select your year"
-                  isDisabled={true}
-                  value=""
-                />
-                <SelectItem label="Remedial" value="remedial" />
                 <SelectItem label="1" value="1" />
                 <SelectItem label="2" value="2" />
                 <SelectItem label="3" value="3" />
                 <SelectItem label="4" value="4" />
-                <SelectItem label="5" value="5" />
               </SelectContent>
             </SelectPortal>
           </Select>
         </View>
 
-        <Link href="/(tabs)" asChild>
-          <Button size="xl">
-            <ButtonText>Submit Information</ButtonText>
-          </Button>
-        </Link>
+        {/* Submit Button */}
+        <Button size="xl" onPress={handleSubmit}>
+          <ButtonText>Submit Information</ButtonText>
+        </Button>
       </View>
     </View>
   );
