5889d2425dc26186852ca991464ff84772ec0839
nati <nsisay49@gmail.com>
2025-08-01 09:22:43 -0600

    further study required

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,12 @@
 import { Redirect, Stack } from "expo-router";
 import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
-import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache}>
-      <GluestackUIProvider mode="light">
-        <SignedIn>
-          <Redirect href="/(tabs)" />
-        </SignedIn>
-        <SignedOut>
-          <Stack screenOptions={{ headerShown: false }} />
-        </SignedOut>
-      </GluestackUIProvider>
-    </ClerkProvider>
+    <GluestackUIProvider mode="light">
+      <Stack screenOptions={{ headerShown: false }} />
+    </GluestackUIProvider>
   );
 }
